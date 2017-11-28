5922418a51592edac395df1112f1f53d83bd7d40
Mikael Korpela <mikael@ihminen.org>
2017-11-28 18:04:34 +0200

    STR fix: lib/accept

--- client/lib/accept/index.js
+++ client/lib/accept/index.js
@@ -12,14 +12,7 @@ import React from 'react';
  */
 import AcceptDialog from './dialog';
 
-export default function(
-	context,
-	message,
-	callback,
-	confirmButtonText,
-	cancelButtonText,
-	options
-) {
+export default function( message, callback, confirmButtonText, cancelButtonText, options ) {
 	var wrapper = document.createElement( 'div' );
 	document.body.appendChild( wrapper );
 
